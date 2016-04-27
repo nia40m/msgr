df3351ee08a35ebb4231f0af764c7f65e4d99a21
Sokolov Nikita <nia40m@yandex.ru>
2016-04-27 22:24:17 +0300

    Now we have simple server that currently can't do anything.
    Signed-off-by: Sokolov Nikita <nia40m@yandex.ru>

--- server.c
+++ server.c
@@ -6,16 +6,19 @@
 int main(void)
 {
 	int smpl_sckt;
+	int incom_sckt;
 	int status;
 
-	struct in_addr in_ip;
-	struct sockaddr_in in_sock;
+	struct in_addr ip;
+	struct sockaddr_in srvr_sckt;
+	struct sockaddr_in clnt_sckt;
+	socklen_t sckt_lngth = (socklen_t) sizeof(struct sockaddr_in);
 
 /* information for binding */
-	in_ip.s_addr = htonl(INADDR_ANY);
-	in_sock.sin_family = AF_INET;
-	in_sock.sin_port = htons(1488);
-	in_sock.sin_addr = in_ip;
+	ip.s_addr = htonl(INADDR_ANY);
+	srvr_sckt.sin_family = AF_INET;
+	srvr_sckt.sin_port = htons(1488);
+	srvr_sckt.sin_addr = ip;
 
 /* first step is to make simple socket */
 	smpl_sckt = socket(AF_INET, SOCK_STREAM, 0);
@@ -25,10 +28,31 @@ int main(void)
 	}
 
 /* binding socket */
-	status = bind(smpl_sckt, (struct sockaddr *) &in_sock, sizeof(in_sock));
+	status = bind(smpl_sckt, (struct sockaddr *) &srvr_sckt, sckt_lngth);
 	if (status == -1) {
 		perror("Can't bind simple socket");
+		close(smpl_sckt);
 		return -1;
 	}
 
+/* making a queue for incoming connections */
+	status = listen(smpl_sckt, 9);
+	if (status == -1) {
+		perror("Can't make queue");
+		close(smpl_sckt);
+		return -1;
+	}
+
+/* listenning to incoming connections */
+	incom_sckt = accept(smpl_sckt,
+		(struct sockaddr *) &clnt_sckt,
+		&sckt_lngth);
+	if (incom_sckt == -1) {
+		perror("Can't accept incoming socket");
+		close(smpl_sckt);
+		return -1;
+	}
+
+/* close socket */
+	close(smpl_sckt);
 }
